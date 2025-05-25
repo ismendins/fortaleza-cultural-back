ea2f7d26f18995c8a093bdbf7444b13e0453e814
Isabella Mendes <isabelammartins7@gmail.com>
2025-05-25 16:49:11 -0300

    Initial commit

---
+++ src/main/java/fortcultural/arquitetura/FortalezaculturalApplication.java
@@ -0,0 +1,13 @@
+package fortcultural.arquitetura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortalezaculturalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortalezaculturalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fortcultural/arquitetura/FortalezaculturalApplicationTests.java
@@ -0,0 +1,13 @@
+package fortcultural.arquitetura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortalezaculturalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
